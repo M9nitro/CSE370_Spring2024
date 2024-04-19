a2ff94bc10cc82b814d1be53e1645869b8d3bfbb
Marzanul Momenine <120085817+M9nitro@users.noreply.github.com>
2024-04-19 20:05:15 +0600

    Initial login/ Signup

---
+++ furever_pet_center/signin.php
@@ -0,0 +1,29 @@
+<?php
+// first of all, we need to connect to the database
+require_once('DB_connect.php');
+
+// we need to check if the input in the form textfields are not empty
+if(isset($_POST['userID']) && isset($_POST['pass'])){
+	// write the query to check if this username and password exists in our database
+	$user = $_POST['userID'];
+	$pass = $_POST['pass'];
+	$sql_query = "SELECT * FROM user WHERE userID = '$user' AND user_password = '$pass'";
+	
+	//Execute the query 
+	$result = mysqli_query($connection_status, $sql_query);
+	
+	//check if it returns an empty set
+	if(mysqli_num_rows($result) !=0 ){
+	
+		echo "LET HIM ENTER";
+		//header("Location: home.php");
+	}
+	else{
+		echo "Username or Password is wrong";
+		//header("Location: index.php");
+	}
+	
+}
+
+
+?>
