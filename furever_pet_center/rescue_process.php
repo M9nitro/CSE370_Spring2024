ce510b3cad82ea5187018fc494761221dc751a96
Meheruba Hasin <87016852+meherubahasin@users.noreply.github.com>
2024-04-25 21:09:18 +0600

    rescue_process.php

--- furever_pet_center/rescue_process.php
+++ furever_pet_center/rescue_process.php
@@ -25,9 +25,9 @@ function who($string){
     } 
 }
 
-echo "Hello";
-if (isset($_POST['petID']) && isset($_POST['pet_type']) && isset($_POST['rescuerID']) && isset($_POST['pet_name']) && isset($_POST['vet_report']) && isset($_POST['pet_age']) && isset($_POST['pet_breed']) && isset($_POST['rescue_date'])) {
 
+if (isset($_POST['petID']) && isset($_POST['pet_type']) && isset($_POST['rescuerID']) && isset($_POST['pet_name']) && isset($_POST['vet_report']) && isset($_POST['pet_age']) && isset($_POST['pet_breed']) && isset($_POST['past_owners'])) {
+echo "hello";
 $petID = $_POST['petID'];
 $pet_type = who('pet_type');
 $rescuerID = $_POST['rescuerID'];
@@ -35,10 +35,13 @@ $pet_name = $_POST['pet_name'];
 $vet_report = nullORnot('vet_report');
 $pet_age = $_POST['pet_age'];
 $pet_breed = $_POST['pet_breed'];
+$pet_owners = explode(" ", $_POST['past_owners']);
+
+
 
 
 $sql_check = "SELECT * FROM pet WHERE petID = '$petID'";
-$count = mysqli_num_rows(mysqli_query($conn, $sql_check));
+$count = mysqli_num_rows(mysqli_query($connection_status, $sql_check));
 
 if ($count > 0) {
 
@@ -47,25 +50,29 @@ if ($count > 0) {
        console.log('Pet Already Exists');
     </script>";
     
-    header("Location: rescue.html");
+    header("Location: rescue.php");
      exit();
 }
 
 
-$sql = " INSERT INTO pet VALUES ( '$petID',  '$rescuerID',  '$pet_name', '$pet_age', '$pet_breed', '$pet_type', '$vet_report', '$rescue_date') ";
+$sql = " INSERT INTO pet_db VALUES ( '$petID',  '$rescuerID',  '$pet_name', '$pet_age', '$pet_breed', '$pet_type', '$vet_report', '$rescue_date') ";
 
 //Execute the query 
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($connection_status, $sql);
 
+for($x = 0; $x < sizeof($pet_owners); $x++){
+    $sql = "INSERT INTO past_petowner VALUES('$petID', '$pet_owners[$x]')";
+    $result = mysqli_query($connection_status, $sql);
+}
 //check if this insertion is happening in the database
-if(mysqli_affected_rows($conn)){
+if(mysqli_affected_rows($connection_status)){
 	
     echo "<script>alert('Inserted Successfully')</script>";
-    header("Location: rescue.html");
+    header("Location: rescue.php");
 }
 else{
     echo '<script>alert("Pet Already exits! or invalid input")</script>'; 
-    header("Location: rescue.html");
+    header("Location: rescue.php");
 }
 
 }
